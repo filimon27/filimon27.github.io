87586420bde1937161d90c9dd141d1c3006e7af4
Filimon Bihonegn <filimonmarkos@gmail.com>
2022-02-28 13:55:52 -0600

    done

---
+++ classWork/objectDemo.js
@@ -0,0 +1,71 @@
+
+const student1={
+    studentId: "s101",
+    quizAnswers:[1,1,2,4]
+}
+const student2={
+    studentId : "s102",
+    quizAnswers:[2,1,2,2]
+}
+
+const student3={
+    studentId : "s103",
+    quizAnswers:[3,1,3,4]
+}
+ let quiz=[ student1,student2,student3];
+ 
+ /**
+  * 
+  * @param {*} quiz 
+  * @param {*} correctAnswers 
+  * @returns 
+  */
+
+ function gradeQuiz(quizArr,correctAnswers){
+let scores=[];
+
+    for(let i=0;i<quizArr.length;i++){
+        let count=0
+        for(let j=0;j<quizArr[i].quizAnswers.length;j++){
+        if(quizArr[i].quizAnswers[j]===correctAnswers[j]){
+              count=count+1
+            }
+           
+              
+    }
+    scores.push(count)
+  
+ }  
+ return scores;
+
+}
+
+/**
+ * 
+ * const numbers = { one: 1,
+two: 22, three: 333, four: 444
+}
+const properties = ["one", "two", "three", "four"];
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+const numbers = { one: 1,
+    two: 22, three: 333, four: 444
+    }
+    const properties = ["one", "two", "three", "four"]
+         
+      for(let i=0;i<properties.length;i++){
+         
+          console.log(i)
+      }
+
+
+
+ console.log(gradeQuiz(quiz,[3,1,2,4]));
+
+
+
+ 
